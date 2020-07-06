e0526f3ed04e8521386c3037d6bbcb0108458303
overf10w <pvvner@yandex.ru>
2020-07-06 11:05:29 +0300

    Import _MK_Glow

---
+++ Assets/_MK/MKGlow/Scripts/CameraData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MK.Glow
+{
+    /// <summary>
+    /// Pipeline Independent pass of necessary camera data
+    /// </summary>
+    internal abstract class CameraData
+    {
+        internal int width, height;
+        internal bool stereoEnabled;
+        internal float aspect;
+        internal Matrix4x4 worldToCameraMatrix;
+    }
+}
